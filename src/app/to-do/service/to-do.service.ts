6910bea137fbd3475c2d5ba1001fa4d1fcdc055d
Jeroenouw <jeroenouw@hotmail.com>
2017-06-21 13:36:53 +0200

    Add and Get added in ToDoService

--- src/app/to-do/service/to-do.service.ts
+++ src/app/to-do/service/to-do.service.ts
@@ -3,7 +3,29 @@ import { ToDo } from '../model/to-do.model';
 
 @Injectable()
 export class ToDoService {
+  lastId: number = 0;
+  todos: ToDo[] = [];
 
   constructor() { }
+  
+  // Create/Post
+  addTodo(todo: ToDo): ToDoService {
+    if (!todo.id) {
+      todo.id = ++this.lastId;
+    }
+    this.todos.push(todo);
+    return this;
+  }
+  
+  // Read/Get
+  getAllTodos(): ToDo[] {
+    return this.todos;
+  }
+
+  getTodoById(id: number): ToDo {
+    return this.todos
+      .filter(todo => todo.id === id)
+      .pop();
+  }
 
 }
