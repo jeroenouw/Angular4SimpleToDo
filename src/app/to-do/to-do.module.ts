d0b1dabbefceebaa0d4cf0f758e54c51fe0377b3
Jeroenouw <jeroenouw@hotmail.com>
2017-06-21 14:14:58 +0200

    Minor changes

--- src/app/to-do/to-do.module.ts
+++ src/app/to-do/to-do.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCheckboxModule, MdInputModule, MaterialModule } from '@angular/material';
 
 import { ToDoService } from './service/to-do.service'
 import { ToDoComponent } from './component/to-do.component'
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule, MdCheckboxModule, MdInputModule, MaterialModule
   ],
   declarations: [
     ToDoComponent
