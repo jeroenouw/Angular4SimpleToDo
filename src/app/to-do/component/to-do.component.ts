f465e37d94d17547e345bc572bbdaf83cd216677
Jeroenouw <jeroenouw@hotmail.com>
2017-06-21 13:54:31 +0200

    Component ts update

--- src/app/to-do/component/to-do.component.ts
+++ src/app/to-do/component/to-do.component.ts
@@ -8,11 +8,25 @@ import { ToDoService } from '../service/to-do.service';
   styleUrls: ['./to-do.component.scss']
 })
 export class ToDoComponent implements OnInit {
+  newTodo: ToDo = new ToDo();
 
   constructor(private todoService: ToDoService) {
   }
 
   ngOnInit() {
+  }
+
+  addTodo() {
+    this.todoService.addTodo(this.newTodo);
+    this.newTodo = new ToDo();
+  }
+
+  removeTodo(todo) {
+    this.todoService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoService.getAllTodos();
   }
 
 }
